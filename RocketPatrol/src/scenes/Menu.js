8ffb6e10c4fc036fd0e3f122bfb0787a9cfedf27
Stryker Buffington <sbuffing@ucsc.edu>
2021-04-14 17:55:46 -0700

    stuff

--- RocketPatrol/src/scenes/Menu.js
+++ RocketPatrol/src/scenes/Menu.js
@@ -5,9 +5,9 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.audio('sfx_select', './RocketPatrol/assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './RocketPatrol/assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './RocketPatrol/assets/rocket_shot.wav');
     }
 
     create() {
