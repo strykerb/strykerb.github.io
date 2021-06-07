9f773efc68d26335fb735ed8e8b0323dee889050
Stryker Buffington <sbuffing@ucsc.edu>
2021-06-06 21:25:35 -0700

    prog5

--- prog5/src/asg5.js
+++ prog5/src/asg5.js
@@ -251,7 +251,7 @@ function sessionStarted(session) {
   let debug = createCylinder(8, [0.5, 0, 0.7]);
   debug.setRotate(0, 180, 0);
   debug.setScale(0.1, 0.1, 0.1);
-  debug.setTranslate(0.5, 0, -1);
+  debug.setTranslate(0, 0, -1);
 
   let mul = 1;
   let move = -12;
@@ -602,7 +602,7 @@ function renderScene(gl, view, programInfo, buffers, texture, deltaTime) {
     modelMatrix.rotate(cylinder.rotate[1], 0, 1, 0);
     modelMatrix.rotate(cylinder.rotate[2], 0, 0, 1);
 
-    modelMatrix.multiply(rotMatrix);
+    modelMatrix = modelMatrix.multiply(rotMatrix);
 
     // Apply scaling for this cylinder
     modelMatrix.scale(cylinder.scale[0], cylinder.scale[1], cylinder.scale[2]);
